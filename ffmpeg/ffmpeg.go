dd39ac469ceb2c8a7506c891d3dd9e2bfff5a2bd
Hell0wor1d <hell0wor1d>
2014-11-30 22:17:16 +0800

    updates

--- ffmpeg/ffmpeg.go
+++ ffmpeg/ffmpeg.go
@@ -1,51 +1,113 @@
+/*
+Author Kev7n
+Email root@kev7n.com
+https://github.com/Hell0wor1d/go2ffmpeg
+
+MIT
+*/
 package ffmpeg
 
 import (
-	"bytes"
-	"encoding/binary"
+	//"bytes"
+	//	"encoding/binary"
 	"io"
 	"log"
+	"os"
 	"os/exec"
 )
 
-type Ffmpeg struct {
-	in  io.ReadCloser
-	Cmd *exec.Cmd
+type FFmpeg struct {
+	cmd *exec.Cmd
 }
 
-func NewFfmpeg(filename string) *Ffmpeg {
-	cmd := exec.Command("ffmpeg", "-i", filename, "-f", "s16le", "-")
-	stdout, err := cmd.StdoutPipe()
+func init() {
+	// check if ffmpeg file exists.
+	f, err := exec.LookPath("ffmpeg")
+	if err != nil {
+		log.Fatal("ffmpeg dose not install.", err)
+	}
+	log.Println("ffmpeg path: ", f)
+}
+
+func NewFFmpeg(args []string) *FFmpeg {
+	cmd := exec.Command("ffmpeg", args...) // "-i", filename, "-f", "s15le", "-")
+	//	stdout, err := cmd.StdoutPipe()
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	//	stdin, err := cmd.StdinPipe()
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	//	stderr, err := cmd.StderrPipe()
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	//	if err := cmd.Start(); err != nil {
+	//		log.Fatal(err)
+	//	}
+	return &FFmpeg{cmd}
+}
+
+func (f *FFmpeg) Write() error {
+	return nil
+}
+
+func (f *FFmpeg) Read(outWriter io.Writer) error {
+	stdout, err := f.cmd.StdoutPipe()
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := cmd.Start(); err != nil {
+
+	stderr, err := f.cmd.StderrPipe()
+	if err != nil {
 		log.Fatal(err)
 	}
-	return &Ffmpeg{stdout, cmd}
-}
 
-func (f *Ffmpeg) Close() error {
-	return f.in.Close()
-}
-
-func (e *Ffmpeg) ProcessAudio(out [][]int16) {
-	// int16 takes 2 bytes
-	bufferSize := len(out[0]) * 4
-	var pack = make([]byte, bufferSize)
-	if _, err := e.in.Read(pack); err != nil {
+	err = f.cmd.Start()
+	if err != nil {
 		log.Fatal(err)
 	}
-	n := make([]int16, len(out[0])*2)
-	for i := range n {
-		var x int16
-		buf := bytes.NewBuffer(pack[2*i : 2*(i+1)])
-		binary.Read(buf, binary.LittleEndian, &x)
-		n[i] = x
-	}
+	defer f.wait()
+	//go func() {
+	//	for n, err = stdout.Read(buffer); err == nil; n, err = stdout.Read(buffer) {
+	//		log.Printf("%s\n", buffer[:n])
+	//	}
+	//}()
+	go io.Copy(outWriter, stdout)
+	go io.Copy(os.Stdout, stderr)
+	return err
+}
 
-	for i := range out[0] {
-		out[0][i] = n[2*i]
-		out[1][i] = n[2*i+1]
-	}
+func (f *FFmpeg) wait() error {
+	return f.cmd.Wait()
+}
+
+func (f *FFmpeg) Close() error {
+	return nil
+}
+
+func (e *FFmpeg) ProcessAudio(out [][]int16) {
+	//	// int16 takes 2 bytes
+	//	bufferSize := len(out[0]) * 4
+	//	var pack = make([]byte, bufferSize)
+	//	if _, err := e.Out.Read(pack); err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	n := make([]int16, len(out[0])*2)
+	//	for i := range n {
+	//		var x int16
+	//		buf := bytes.NewBuffer(pack[2*i : 2*(i+1)])
+	//		binary.Read(buf, binary.LittleEndian, &x)
+	//		n[i] = x
+	//	}
+	//
+	//	for i := range out[0] {
+	//		out[0][i] = n[2*i]
+	//		out[1][i] = n[2*i+1]
+	//	}
 }
--- go2ffmpeg.go
+++ go2ffmpeg.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"bytes"
-	"code.google.com/p/portaudio-go/portaudio"
-	"encoding/binary"
+	//"code.google.com/p/portaudio-go/portaudio"
 	"fmt"
-	"io"
+	"github.com/Hell0wor1d/go2ffmpeg/ffmpeg"
 	"log"
 	"os"
-	"os/exec"
 )
+
+func init() {
+	fmt.Printf("init app.")
+}
 
 func main() {
 	if len(os.Args) <= 1 {
@@ -17,66 +18,33 @@ func main() {
 		os.Exit(0)
 	}
 	inputfile := os.Args[1]
-	chk := func(err error) {
-		if err != nil {
-			panic(err)
-		}
+	//	chk := func(err error) {
+	//		if err != nil {
+	//			panic(err)
+	//		}
+	//	}
+	//	framePerBuffer := 2048
+	args := []string{
+		"-i", inputfile,
+		"-f", "s16le",
+		"-",
 	}
-	framePerBuffer := 2048
-	ff := newFfmpeg(inputfile)
-	defer ff.Close()
-
-	portaudio.Initialize()
-	defer portaudio.Terminate()
-	stream, err := portaudio.OpenDefaultStream(0, 2, 44100, framePerBuffer, ff)
-
-	chk(err)
-	defer stream.Close()
-	chk(stream.Start())
-	if err := ff.cmd.Wait(); err != nil {
-		log.Fatal(err)
+	ff := ffmpeg.NewFFmpeg(args)
+	if ff == nil {
+		log.Fatal("ffmpeg can not be nill.")
 	}
-	chk(stream.Stop())
-}
+	//defer ff.Close()
+	buffer := make([]byte, 1024)
 
-type ffmpeg struct {
-	in  io.ReadCloser
-	cmd *exec.Cmd
-}
-
-func newFfmpeg(filename string) *ffmpeg {
-	cmd := exec.Command("ffmpeg", "-i", filename, "-f", "s16le", "pipe:1")
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-	return &ffmpeg{stdout, cmd}
-}
-
-func (f *ffmpeg) Close() error {
-	return f.in.Close()
-}
-
-func (e *ffmpeg) ProcessAudio(_, out [][]int16) {
-	// int16 takes 2 bytes
-	bufferSize := len(out[0]) * 4
-	var pack = make([]byte, bufferSize)
-	if _, err := e.in.Read(pack); err != nil {
-		log.Fatal(err)
-	}
-	n := make([]int16, len(out[0])*2)
-	for i := range n {
-		var x int16
-		buf := bytes.NewBuffer(pack[2*i : 2*(i+1)])
-		binary.Read(buf, binary.LittleEndian, &x)
-		n[i] = x
-	}
-
-	for i := range out[0] {
-		out[0][i] = n[2*i]
-		out[1][i] = n[2*i+1]
-	}
+	ff.Read(buffer)
+	//	portaudio.Initialize()
+	//	defer portaudio.Terminate()
+	//	stream, err := portaudio.OpenDefaultStream(0, 2, 44100, framePerBuffer, ff)
+	//chk(err)
+	//defer stream.Close()
+	//chk(stream.Start())
+	//if err := ff.Wait(); err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	chk(stream.Stop())
 }
